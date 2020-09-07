989a76ee98647ed5b2fd1238262dd8f8e2870d9a
= <=>
2020-09-07 06:41:07 +0000

    邮箱验证功能

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', 'PagesController@root')->name('root');
 
-    Route::get('/', 'PagesController@root')->name('root');
-
-Auth::routes();
+Auth::routes(['verify' => true]);
 
 Route::get('/home', 'HomeController@index')->name('home');
