ca6c9bdb4a59cd5a4d8d16947aa59dc7b98779e9
= <=>
2020-09-07 06:32:45 +0000

    注册与登录

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'PagesController@root')->name('root');
+    Route::get('/', 'PagesController@root')->name('root');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
