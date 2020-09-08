49a6464dc6bff19d48d2f0f0c38d719465ce2357
= <=>
2020-09-08 05:58:42 +0000

    购物车页面

--- routes/web.php
+++ routes/web.php
@@ -38,6 +38,8 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
 
     // 购物车
     Route::post('cart', 'CartController@add')->name('cart.add');
+    Route::get('cart', 'CartController@index')->name('cart.index');
+    Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
 
 });
 
