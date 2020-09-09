9f0e241a8a621f13099b95c6e6e9f176e289e9e8
= <=>
2020-09-09 06:51:04 +0000

    管理后台 - 订单发货

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -12,15 +12,19 @@ Route::group([
 
     $router->get('/', 'HomeController@index')->name('admin.home');
     $router->get('users', 'UsersController@index');
+
+    // 商品管理
     $router->get('products', 'ProductsController@index');
     $router->get('products/create', 'ProductsController@create');
     $router->post('products', 'ProductsController@store');
     $router->get('products/{id}/edit', 'ProductsController@edit');
     $router->put('products/{id}', 'ProductsController@update');
+
+    // 订单管理
     $router->get('orders', 'OrdersController@index')->name('admin.orders.index');
     $router->get('orders/{order}', 'OrdersController@show')->name('admin.orders.show');
+    $router->post('orders/{order}/ship', 'OrdersController@ship')->name('admin.orders.ship');
 
 
 
-    
 });
