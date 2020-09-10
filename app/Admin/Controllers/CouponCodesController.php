ec31dc0d964140b5bbe926cb624565c547d3501b
= <=>
2020-09-10 01:29:19 +0000

    用户界面 - 使用优惠券下单

--- app/Admin/Controllers/CouponCodesController.php
+++ app/Admin/Controllers/CouponCodesController.php
@@ -26,27 +26,18 @@ class CouponCodesController extends AdminController
     {
         $grid = new Grid(new CouponCode);
 
-        // 默认按创建时间倒序排序
         $grid->model()->orderBy('created_at', 'desc');
         $grid->id('ID')->sortable();
         $grid->name('名称');
         $grid->code('优惠码');
         $grid->description('描述');
-        $grid->type('类型')->display(function($value) {
-            return CouponCode::$typeMap[$value];
+        $grid->column('usage', '用量')->display(function ($value) {
+            return "{$this->used} / {$this->total}";
         });
-        // 根据不同的折扣类型用对应的方式来展示
-        $grid->value('折扣')->display(function($value) {
-            return $this->type === CouponCode::TYPE_FIXED ? '￥'.$value : $value.'%';
-        });
-        $grid->min_amount('最低金额');
-        $grid->total('总量');
-        $grid->used('已用');
-        $grid->enabled('是否启用')->display(function($value) {
+        $grid->enabled('是否启用')->display(function ($value) {
             return $value ? '是' : '否';
         });
         $grid->created_at('创建时间');
-
         $grid->actions(function ($actions) {
             $actions->disableView();
         });
