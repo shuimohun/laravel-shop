7276ee771dd05a52802d21893fe602351ca6e8b7
= <=>
2020-09-11 03:19:34 +0000

    前台分期付款列表页

--- routes/web.php
+++ routes/web.php
@@ -65,6 +65,7 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::get('payment/{order}/wechat', 'PaymentController@payByWechat')->name('payment.wechat');
     // 分期支付
     Route::post('payment/{order}/installment', 'PaymentController@payByInstallment')->name('payment.installment');
+    Route::get('installments', 'InstallmentsController@index')->name('installments.index');
 
     // 优惠券
     Route::get('coupon_codes/{code}', 'CouponCodesController@show')->name('coupon_codes.show');
